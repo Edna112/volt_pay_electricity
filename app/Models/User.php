c776ee8542bb41c7129cb753be65165e990124a1
Edna112 <chenwieddy@gmail.com>
2026-04-29 22:24:34 +0100

    Update User model to include 'two_factor_secret' in hidden attributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,7 +32,6 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $hidden = ['password', 'remember_token'];
     protected $hidden = ['password', 'remember_token', 'two_factor_secret'];
     /**
      * Get the attributes that should be cast.
